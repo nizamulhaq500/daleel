4e9758051216cbd50651368ca85917e51dd49dfd
Nizam ul haq <nizamulhaq500@gmail.com>
2026-08-24 14:59:26 +0530

    Revert NewsTicker to previous state and fix photo background opacities

--- src/components/DynamicBackground.tsx
+++ src/components/DynamicBackground.tsx
@@ -3,11 +3,11 @@
 import { useEffect, useRef } from 'react';
 
 const PHOTOS = [
-  { src: '/assets/delhi-mosque.jpg', top: '5%', left: '5%', width: '35vw', height: '40vh', rotate: '-2deg', opacity: 0.18 },
-  { src: '/assets/jakarta-mosque.jpg', top: '10%', left: '45%', width: '30vw', height: '35vh', rotate: '1.5deg', opacity: 0.22 },
-  { src: '/assets/praying-people.jpg', top: '50%', left: '10%', width: '40vw', height: '38vh', rotate: '3deg', opacity: 0.15 },
-  { src: '/assets/flags-mosque.jpg', top: '45%', left: '60%', width: '35vw', height: '42vh', rotate: '-1.5deg', opacity: 0.20 },
-  { src: '/assets/dhaka-protest.jpg', top: '80%', left: '30%', width: '42vw', height: '30vh', rotate: '2deg', opacity: 0.16 },
+  { src: '/assets/delhi-mosque.jpg', top: '5%', left: '5%', width: '35vw', height: '40vh', rotate: '-2deg', opacity: 0.45 },
+  { src: '/assets/jakarta-mosque.jpg', top: '10%', left: '45%', width: '30vw', height: '35vh', rotate: '1.5deg', opacity: 0.55 },
+  { src: '/assets/praying-people.jpg', top: '50%', left: '10%', width: '40vw', height: '38vh', rotate: '3deg', opacity: 0.40 },
+  { src: '/assets/flags-mosque.jpg', top: '45%', left: '60%', width: '35vw', height: '42vh', rotate: '-1.5deg', opacity: 0.50 },
+  { src: '/assets/dhaka-protest.jpg', top: '80%', left: '30%', width: '42vw', height: '30vh', rotate: '2deg', opacity: 0.42 },
 ];
 
 function PhotoGrid() {
